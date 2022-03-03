d9671eec2cc349425a3d30691a8fa4831db1f681
owopi <awesomebuilds.playmc@gmail.com>
2022-03-02 23:38:25 -0500

    aaa

--- api/api.mjs
+++ api/api.mjs
@@ -1,11 +1,24 @@
 const userCardTemplate = document.querySelector("[data-user-template]")
 const userCardContainer = document.querySelector("[data-user-container]")
 
+/* Search bar */
+
+let f_a_q = [];
+const inputBar = document.getElementById("search");
+inputBar.addEventListener("input", e => {
+    const value = e.target.value;
+    f_a_q.forEach(faq => {
+        const isVisible = faq.question.includes(value) || faq.description.includes(value)
+        faq.element.classList.toggle("hide", !isVisible)
+    });
+console.log(users)
+})
+
 /* Api to get the faq */
 fetch("https://vlxtiykg.github.io/Api-for-page/api/search.json")
     .then(response => response.json())
     .then( data => {
-        search = data.map(faq => {
+        f_a_q = data.map(faq => {
             /* clone card and give document fragment */
             const card = userCardTemplate.content.cloneNode(true).children[0]
             /* Find the data attributes of the two cards */
@@ -23,9 +36,6 @@ fetch("https://vlxtiykg.github.io/Api-for-page/api/search.json")
         });
     })
 
-    /* Search bar */
-let f_a_q = [];
-const inputBar = document.getElementById("search")
 
 
 document.addEventListener("click", e => {
